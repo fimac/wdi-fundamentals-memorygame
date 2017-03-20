8df49932d8c167ae172dd07bd8fa803627d2bcf0
Fiona McCawley <fionamccawley@Fionas-MacBook-Pro.local>
2017-03-20 16:38:38 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -18,11 +18,14 @@ var cards = [
 {
 	rank: "king",
 	suit: "diamonds",
-	cardImage: "images/king-of-hearts"
+	cardImage: "images/king-of-diamonds.png"
 }
 ];
 
 var cardsInPlay = [];
+
+
+//function to check if 2 cards have been clicked and if they match
 
 var checkForMatch = function () {
 	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
@@ -31,18 +34,32 @@ var checkForMatch = function () {
 	else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
 		alert("Sorry, try again");
 	}
-}
+};
 
-var flipCard = function (cardId) {
+//function to run through card flip when clicked
+
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 console.log("User flipped " + cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
-}
+};
 
-flipCard(0);
-flipCard(2);
+
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+}
+};
+
+createBoard();
 
 
 
